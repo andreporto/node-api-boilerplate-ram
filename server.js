6317adb40be3a3b1ec8b3d2ee1baac890dab5bbf
André de Alencar Porto <andreporto@yahoo.com>
2019-10-14 22:54:46 -0300

    add post method and validation middlewares

--- server.js
+++ server.js
@@ -7,6 +7,36 @@ const server = express();
 // set server to read/write json
 server.use(express.json());
 
+// middleware to validade existing users for GET/PUT/DELETE methods
+function validadeExistingUser(req, res, next) {
+  if (!data[req.params.index]) {
+    return res.status(404).json({ error: 'User does not exists.' });
+  }
+
+  next();
+}
+
+// middleware to validade user data for PUT/POST methods
+function validateUserData(req, res, next) {
+  if (!req.body.name) {
+    return res.status(400).json({ error: 'User is required.' });
+  }
+
+  next();
+}
+
+// implementing of a global middleware
+function log(req, res, next) {
+  console.time();
+  console.log(`${req.method}; ${req.url}`);
+  console.timeEnd();
+
+  next();
+}
+
+// using a global middleware as a log
+server.use(log);
+
 // data in memory
 let data = ["Fulano", "Cicrano", "Beltrano"];
 
@@ -16,13 +46,21 @@ server.get("/users", (req, res) => {
 });
 
 // get a particular user by it's index
-server.get("/users/:index", (req, res) => {
+server.get("/users/:index", validadeExistingUser, (req, res) => {
   const index = req.params.index;
   return res.json(data[index]);
 });
 
+// add a new user
+server.post("/users", validateUserData, (req, res) => {
+  const { name } = req.body;
+  data.push(name);
+
+  return res.json(data);
+});
+
 // update a particular user data
-server.put("/users/:index", (req, res) => {
+server.put("/users/:index", validadeExistingUser, validateUserData, (req, res) => {
   const index = req.params.index;
   const { name } = req.body;
 
@@ -32,7 +70,7 @@ server.put("/users/:index", (req, res) => {
 });
 
 // delete a particular user
-server.delete("/users/:index", (req, res) => {
+server.delete("/users/:index", validadeExistingUser, (req, res) => {
   const index = req.params.index;
   data.splice(index, 1);
 
